2eb17cb2e32a5a39258f52c89114c267da0d80ad
quang-star <nguyenkimquang1612@gmail.com>
2026-07-01 14:38:32 +0700

    refactor: improve user service

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,11 +2,11 @@ using Microsoft.AspNetCore.Mvc;
 using Services.Interfaces;
 using DTOs.User;
 using Microsoft.AspNetCore.Authorization;
-
+using FluentValidation;
 namespace Controllers;
 
 [ApiController]
-// [Authorize]
+[Authorize]
 [Route("api/[controller]")]
 public class UserController : ControllerBase
 {
@@ -23,54 +23,36 @@ public class UserController : ControllerBase
     {
         var users = await _userService.GetAllUsersAsync();
         return Ok(users);
+
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<UserDto?>> GetUserById(int id)
     {
         var user = await _userService.GetUserByIdAsync(id);
-        if (user == null)
-        {
-            return NotFound();
-        }
         return Ok(user);
     }
 
     [HttpPost]
-    public async Task<ActionResult<CreateUserDto?>> CreateUser(CreateUserDto userDto)
+    public async Task<ActionResult> CreateUser(CreateUserDto userDto)
     {
+
         var createdUser = await _userService.CreateUserAsync(userDto);
-        if (createdUser == null)
-        {
-            return BadRequest();
-        }
         return Ok(createdUser);
     }
 
     [HttpPut("{id}")]
     public async Task<ActionResult> UpdateUser(int id, UpdateUserDto userDto)
     {
-       
-       var user = await _userService.GetUserByIdAsync(id);
-        if (user == null)
-        {
-            return NotFound();
-        }
-
         await _userService.UpdateUserAsync(id, userDto);
         return NoContent();
+
     }
 
-    [Authorize(Roles = "Admin")]
+    /// [Authorize(Roles = "Admin")]
     [HttpDelete("{id}")]
     public async Task<ActionResult> DeleteUser(int id)
     {
-        var user = await _userService.GetUserByIdAsync(id);
-        if (user == null)
-        {
-            return NotFound();
-        }
-
         await _userService.DeleteUserAsync(id);
         return NoContent();
     }
--- Services/Implementations/UserService.cs
+++ Services/Implementations/UserService.cs
@@ -3,21 +3,30 @@ using Models;
 using Repositories.Interfaces;
 using Services.Interfaces;
 using BCrypt.Net;
+using FluentValidation;
 
 namespace Services.Implementations;
 
 public class UserService : IUserService
 {
     private readonly IUserRepository _userRepository;
+    private readonly IValidator<CreateUserDto> _createUserValidator;
+    private readonly IValidator<UpdateUserDto> _updateUserValidator;
 
-    public UserService(IUserRepository userRepository)
+    public UserService(IUserRepository userRepository, IValidator<CreateUserDto> createUserValidator, IValidator<UpdateUserDto> updateUserValidator)
     {
         _userRepository = userRepository;
+        _createUserValidator = createUserValidator;
+        _updateUserValidator = updateUserValidator;
     }
 
     public async Task<List<UserDto>> GetAllUsersAsync()
     {
         var users = await _userRepository.GetAllAsync();
+        if (users == null || !users.Any())
+        {
+            throw new Exception("No users found.");
+        }
         return users.Select(user => new UserDto
         {
             Id = user.Id,
@@ -32,7 +41,7 @@ public class UserService : IUserService
         var user = await _userRepository.GetByIdAsync(id);
         if (user == null)
         {
-            return null;
+            throw new Exception($"User not found");
         }
 
         return new UserDto
@@ -46,6 +55,11 @@ public class UserService : IUserService
 
     public async Task<CreateUserDto?> CreateUserAsync(CreateUserDto userDto)
     {
+        var validationResult = await _createUserValidator.ValidateAsync(userDto);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
         string hashedPassword = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
         var user = new User
         {
@@ -56,22 +70,31 @@ public class UserService : IUserService
         };
 
         var createdUser = await _userRepository.AddAsync(user);
-
+        if (createdUser == null)
+        {
+            throw new Exception("Failed to create user.");
+        }
         return new CreateUserDto
         {
             Name = createdUser.Name,
             Email = createdUser.Email,
             Password = createdUser.Password
         };
-        
+
     }
 
     public async Task UpdateUserAsync(int id, UpdateUserDto userDto)
     {
+
+        var validationResult = await _updateUserValidator.ValidateAsync(userDto);
+        if (!validationResult.IsValid)
+        {
+            throw new ValidationException(validationResult.Errors);
+        }
         var user = await _userRepository.GetByIdAsync(id);
         if (user == null)
         {
-            throw new Exception("User not found");
+            throw new Exception($"User not found");
         }
 
         if (!string.IsNullOrEmpty(userDto.Name))
@@ -81,7 +104,7 @@ public class UserService : IUserService
         if (!string.IsNullOrEmpty(userDto.Email))
         {
             user.Email = userDto.Email;
-        }   
+        }
         if (!string.IsNullOrEmpty(userDto.Password))
         {
             string hashedPassword = BCrypt.Net.BCrypt.HashPassword(userDto.Password);
@@ -102,5 +125,5 @@ public class UserService : IUserService
         await _userRepository.DeleteAsync(user);
     }
 
-    
+
 }
